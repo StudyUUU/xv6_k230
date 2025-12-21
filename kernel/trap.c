eab4b1040c3928b5b0dcb33d40d87994c01fd772
StudyUUU <604878986@qq.com>
2025-12-21 19:27:56 +0800

    【修改】移除系统每3s的打印信息

--- kernel/trap.c
+++ kernel/trap.c
@@ -8,20 +8,9 @@
 
 extern void kernelvec();
 
-// 全局中断统计计数器
-// 由于已经在 entry.S 中实现了 BSS 清零，这些变量在启动时会自动为 0
-uint64 uart_intr_count;
-uint64 timer_intr_count;
-static uint64 last_timer_count;
-
 void trap_init(void) {
     // 设置内核态的中断向量入口
     w_stvec((uint64)kernelvec);
-    
-    // 初始化统计变量
-    uart_intr_count = 0;
-    timer_intr_count = 0;
-    last_timer_count = 0;
 }
 
 // 内核态中断/异常入口程序
@@ -40,14 +29,6 @@ void kerneltrap() {
         if(which_int == 5) { 
             // 重新设置下一次时钟中断的时间 (K230 STIMECMP)
             set_timer(r_time() + CLOCK_INTERVAL);
-            timer_intr_count++;
-
-            // 定期打印中断统计信息（约每 3 秒一次）
-            if(timer_intr_count - last_timer_count >= 300) {
-                printf("[Stats] Timer: %ld | UART IRQs: %ld\n", timer_intr_count, uart_intr_count);
-                last_timer_count = timer_intr_count;
-            }
-            
             // 以后在此处可以添加 yield() 进行进程调度
         } 
         // -------------------------------------------------------
@@ -60,7 +41,6 @@ void kerneltrap() {
             if(irq == UART0_IRQ) {
                 // 如果是 UART0 (IRQ 16)
                 uartintr();
-                uart_intr_count++;
             } 
             else if(irq != 0) {
                 // 处理非预期的其他硬件中断
